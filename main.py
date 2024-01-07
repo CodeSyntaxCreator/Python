0b209fe892209a07546828b9ac2c281dc38205ae
CodeSyntaxCreator <140944489+CodeSyntaxCreator@users.noreply.github.com>
2024-01-07 11:35:09 +0000

    Hello World Program, Variables, Datatypes

---
+++ main.py
@@ -0,0 +1,38 @@
+# Hello World Program
+print("Hello World");
+
+# Variables
+age = 18      # age is of type int
+name = "John" # name is now of type str
+
+# Data Types
+#  * str => text
+x = "Hello World" #str
+#  * int, float, complex => numeric
+x = 20 # int
+x = 20.5 # float
+x = 1j #complex
+#  * list, tuple, range => sequence
+x = ["apple", "banana", "cherry"] #list
+x = ("apple", "banana", "cherry") #tuple
+x = range(6) #range
+#  * dict => mapping
+x = {"name" : "John", "age" : 36} #dict
+#  * set, frozenset => set
+x = {"apple", "banana", "cherry"} #set
+x = frozenset({"apple,", "banana", "cherry"}) #frozenset
+#  * bool +> boolean
+x = True # bool True/False 
+#  * bytes, bytearray, memoryview => binary
+x = b"Hello" #bytes
+x = bytearray(5) #bytearray
+x = memoryview(bytes(5)) #memoryview
+#  * None => NoneType
+x = None # none
+
+# Specific Data Types
+x = str("Hello World") #str
+x = int(6) #int
+x = float(20.5) #float
+x = complex(20j) #complex
+# TODO: Complete specific data types beginning from sequence
