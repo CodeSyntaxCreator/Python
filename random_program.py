a76ac0eb3f131c6636069a97678b8703ea8cd683
CodeSyntaxCreator <140944489+CodeSyntaxCreator@users.noreply.github.com>
2024-01-08 11:33:33 +0000

    Random Number, Random Float Program

---
+++ random_program.py
@@ -0,0 +1,14 @@
+# Random Number Program
+#Importing random module
+import random; #built-in module
+#Declaring randomNum
+randomNum = random.randrange(1,10); #from 1 to 10
+print(randomNum)
+
+#Random Float
+randomFloat = random.random() #Generate a random float from 0 to 1
+print(randomFloat)
+
+#Random Intege
+randomInteger = random.randint(10, 1000) # from 10 to 1000
+print(randomInteger)
