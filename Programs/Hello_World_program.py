99d589d6aafe6d70eccf447867159123bd4cdc5a
CodeSyntaxCreator <140944489+CodeSyntaxCreator@users.noreply.github.com>
2024-01-09 08:25:15 +0000

    Not that much

---
+++ Programs/Hello_World_program.py
@@ -0,0 +1,3 @@
+#Hello World Program
+#Print Hello World
+print("Hello World"); #Semicolons are optional
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# Hello World Program
-print("Hello World");
-
+ # Hello World Program
+#print("Hello World");
 # Variables
 age = 18      # age is of type int
 name = "John" # name is now of type str
